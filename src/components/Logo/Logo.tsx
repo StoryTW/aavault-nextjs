0a78e9d8332c3d00a90cf5a3f272712095b9b502
parienko_sergej <parienko_sergej@enot.work>
2024-07-03 02:54:31 +0300

    fix

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import styles from './Logo.module.scss'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export const Logo = () => {
   return (
-    <div className={styles.logo}>
+    <Link href={'/'} className={styles.logo}>
       <Image
         src={'/images/logo.svg'}
         alt='logo'
         width={213}
         height={50}
       />
-    </div>
+    </Link>
   )
 }
--- src/pageComponents/TradePage/TradePage.tsx
+++ src/pageComponents/TradePage/TradePage.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import styles from './TradePage.module.scss';
-import Image from 'next/image';
 
 export const TradePage = () => {
   return (
     <main className={styles.tradePage}>
-      <h1 className={styles.title}>
-        Soon.
-      </h1>
+      <h1 className={styles.title}>Soon.</h1>
 
-      <div className={styles.robot}/>
-
-
-      {/* <Image
-        src={'/images/robot.png'}
-        alt='robot'
-        height={882}
-        width={1100}
-        className={styles.robot}
-      /> */}
+      <div className={styles.robot} />
     </main>
   );
 };
