bcf6fe444afaa6037b4181ed9dc24ca5957eff81
parienko_sergej <parienko_sergej@enot.work>
2024-07-03 15:50:28 +0300

    fix

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -7,15 +7,15 @@ export const Socials = () => {
   return (
     <div className={styles.socials}>
       <div className={styles.wrapper}>
-        <ButtonLink href={'/'} target='_blank'>
+        <ButtonLink href={'https://twitter.com/AAVAULT_IO'} target='_blank'>
           <Image src={'/images/twitter.svg'} width={24} height={24} alt='twitter' />
         </ButtonLink>
 
-        <ButtonLink href={'/'} target='_blank'>
+        <ButtonLink href={'https://t.me/aavault'} target='_blank'>
           <Image src={'/images/telegram.svg'} width={24} height={24} alt='telegram' />
         </ButtonLink>
 
-        <ButtonLink href={'/'} target='_blank'>
+        <ButtonLink href={'https://discord.gg/BCV39jR8'} target='_blank'>
           <Image src={'/images/discord.svg'} width={24} height={24} alt='discord' />
         </ButtonLink>
       </div>
