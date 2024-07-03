1e8a5a027c9fdf26f2192396a7e3763da20b84b1
parienko_sergej <parienko_sergej@enot.work>
2024-07-03 15:26:22 +0300

    fix

--- src/pageComponents/TradePage/TradePage.tsx
+++ src/pageComponents/TradePage/TradePage.tsx
@@ -5,8 +5,7 @@ export const TradePage = () => {
   return (
     <main className={styles.tradePage}>
       <h1 className={styles.title}>Soon.</h1>
-
-      <div className={styles.robot} />
+      <div className={styles.description}>A little more, and you can trade</div>
     </main>
   );
 };
