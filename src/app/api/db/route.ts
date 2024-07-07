7813ec9324e908966989f1f8d238eb69e07185fe
Timur Gainutdinov <timurga2001@gmail.com>
2024-07-07 20:23:54 +0300

    modified:   src/app/api/db/route.ts

--- src/app/api/db/route.ts
+++ src/app/api/db/route.ts
@@ -3,7 +3,7 @@ import Email from "@/app/models/Email";
 import { NextApiRequest, NextApiResponse } from "next";
 
 
-export default async function POST(req: NextApiRequest, res: NextApiResponse) {    
+export async function POST(req: NextApiRequest, res: NextApiResponse) {    
     dbConnect();
 
     const email = await Email.create(req.body);
