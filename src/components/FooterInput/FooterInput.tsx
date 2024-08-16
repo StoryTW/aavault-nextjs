ca3756f50081f61a3542885ad870ec48471a2bc9
Timur Gainutdinov <timurga2001@gmail.com>
2024-08-16 18:43:57 +0300

    modified:   src/components/FooterInput/FooterInput.tsx

--- src/components/FooterInput/FooterInput.tsx
+++ src/components/FooterInput/FooterInput.tsx
@@ -1,15 +1,30 @@
-'use client'
+'use client';
 import React, { useRef } from 'react';
-import styles from './FooterInput.module.scss'
+import styles from './FooterInput.module.scss';
 import IconArrow from '@/assets/images/arrow.svg';
+import sendMessageToTelegram from '@/utils/sendMessageToTelegram';
 
 export const FooterInput = () => {
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleFocus = () => {
-    if (inputRef.current) {
-      inputRef.current.focus();
+  const handleFocus = async () => {
+    const isEmail = String(inputRef.current?.value)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      );
+
+    if (!inputRef.current?.value) {
+      inputRef.current?.focus();
+      return;
     }
+    if (!isEmail) {
+      inputRef.current.value = '';
+      inputRef.current?.focus();
+    }
+
+    sendMessageToTelegram(inputRef.current?.value);
+    inputRef.current.value = '';
   };
 
   return (
