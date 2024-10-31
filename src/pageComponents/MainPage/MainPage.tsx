e44ef886f7f606dc43a1b2060214465039439016
parienko_sergej <parienko_sergej@enot.work>
2024-11-01 02:34:05 +0300

    fix

--- src/pageComponents/MainPage/MainPage.tsx
+++ src/pageComponents/MainPage/MainPage.tsx
@@ -1,11 +1,11 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import styles from './MainPage.module.scss';
 import { MainSection } from '@/components/Sections/MainSection/MainSection';
 import { FaqSection } from '@/components/Sections/FaqSection/FaqSection';
 import { GreetingsSection } from '@/components/Sections/GreetingsSection/GreetingsSection';
 import { AdvantageSection } from '@/components/Sections/AdvantageSection/AdvantageSection';
 import { NativeCurrencySection } from '@/components/Sections/NativeCurrencySection/NativeCurrencySection';
-import { AavaultSection } from '@/components/Sections/AavaultSection/AavaultSection';
+// import { AavaultSection } from '@/components/Sections/AavaultSection/AavaultSection';
 import { RoadmapSection } from '@/components/Sections/RoadmapSection/RoadmapSection';
 import { PartnersSection } from '@/components/Sections/PartnersSection/PartnersSection';
 
@@ -18,7 +18,7 @@ export const MainPage = () => {
 
       <AdvantageSection />
 
-      <AavaultSection />
+      {/* <AavaultSection /> */}
 
       <NativeCurrencySection />
 
